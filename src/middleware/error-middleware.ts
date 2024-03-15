d55b23990dda2b5a9441be65016ac429f7bb3a72
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-15 22:49:46 +0700

    feat: register user

---
+++ src/middleware/error-middleware.ts
@@ -0,0 +1,24 @@
+import { Response, Request, NextFunction } from "express";
+import { ZodError } from "zod";
+import { ResponseError } from "../error/response-error";
+
+export const ErrorMiddleware = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      errors: `validation error : ${JSON.stringify(error)}`,
+    });
+  } else if (error instanceof ResponseError) {
+    res.status(error.status).json({
+      errors: error.message,
+    });
+  } else {
+    res.status(500).json({
+      erros: error.message,
+    });
+  }
+};
